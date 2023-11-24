782b141197c6ed2efea486d5541233e697104b4e
CesarSustaita <c.sustaita@outlook.com>
2023-11-23 23:14:14 -0600

    Fix: Debug code in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,68 +14,52 @@ const router = createRouter({
     {
       path: '/registro',
       name: 'registro',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+    
       component: () => import('../views/RegistroView1.vue')
     },
     {
       path: '/registro2',
       name: 'registro2',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/RegistroView2.vue')
     },
     {
       path: '/registro3',
       name: 'registro3',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/RegistroView3.vue')
     },
     {
       path: '/registro4',
       name: 'registro4',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/RegistroView4.vue')
     },
     {
       path: '/inicio',
       name: 'inicio',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/InicioSesionView.vue')
     },
     {
       path: '/lector',
       name: 'lector',
       meta: { showNav: false },
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/Analizador/LectorView.vue')
     },
     {
       path: '/analizador',
       name: 'analizador',
       meta: { showNav: false },
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/Analizador/AnalizadorView.vue')
     },
     {
       path: '/analizadorAdmin',
       name: 'analizadorA',
       meta: { showNav: false },
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+
       component: () => import('../views/Analizador/AnalizadorAdminView.vue')
     }
   ]
